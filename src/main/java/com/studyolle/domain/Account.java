40c8afc4c20fb645fd03d8e844141244770345b3
whdcks420 <whdcks420@gmail.com>
2022-09-23 17:52:12 +0900

    Initial commit

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,23 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String username;
+}
