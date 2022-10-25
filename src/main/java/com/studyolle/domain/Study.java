560583a0e54d5c886df8e54d7f06a885aaa2e62c
whdcks420 <whdcks420@gmail.com>
2022-10-25 21:41:42 +0900

    feat/add createStudyClass

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -4,21 +4,27 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Getter @Setter @EqualsAndHashCode(of = "id")
-@Builder @NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Study {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
 
     @ManyToMany
-    private Set<Account> managers;
+    private Set<Account> managers = new HashSet<>();
 
     @ManyToMany
-    private Set<Account> members;
+    private Set<Account> members = new HashSet<>();
 
     @Column(unique = true)
     private String path;
@@ -34,10 +40,10 @@ public class Study {
     private String image;
 
     @ManyToMany
-    private Set<Tag> tags;
+    private Set<Tag> tags = new HashSet<>();
 
     @ManyToMany
-    private Set<Zone> zones;
+    private Set<Zone> zones = new HashSet<>();
 
     private LocalDateTime publishDateTime;
 
@@ -53,4 +59,7 @@ public class Study {
 
     private boolean useBanner;
 
+    public void addManager(Account account) {
+        this.managers.add(account);
+    }
 }
--- src/main/java/com/studyolle/settings/form/NicknameForm.java
+++ src/main/java/com/studyolle/settings/form/NicknameForm.java
@@ -13,6 +13,6 @@ public class NicknameForm {
 
     @NotBlank
     @Length(min = 3, max = 20)
-    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-0_-]{3,20}$")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9_-]{3,20}$")
     private String nickname;
 }
